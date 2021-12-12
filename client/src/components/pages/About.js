5743f43f0722e37ee2d2c7d62e5f60f21ff1e505
bismarkstoney <bismarkstoney@yahoo.com>
2021-12-12 22:02:41 +0000

    add all files

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import logo from './logo.jpeg';
 const About = () => {
 	return (
 		<div>
 			<h1>About</h1>
-			<p className='my-1'>This a fullstact app for keeping contacts</p>
+			<p className='my-1'>This is an app for keeping contacts of Asaw System</p>
 			<p className='bg-dark p'>
 				<strong>Version</strong>: 1.1
 			</p>
+			<span>
+				<img src={logo} alt='' />
+			</span>
 		</div>
 	);
 };
